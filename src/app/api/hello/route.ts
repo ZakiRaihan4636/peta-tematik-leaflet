304e0f6812a44a7b7580dbb952738bdf8106de7d
Cahyagitrama <cahyaramdhn12@gmail.com>
2025-01-10 20:55:03 +0700

    add react

---
+++ src/app/api/hello/route.ts
@@ -0,0 +1,10 @@
+// src/app/api/hello/route.ts
+
+export async function GET() {
+  return new Response(JSON.stringify({ message: 'Hello from Next.js!' }), {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+}
